88ddf1f423020a4f9bed632348870dbfc73b0b15
André Almeida <almeidacandrec@gmail.com>
2022-05-11 11:41:58 -0300

    one more example of dictionary type in python

---
+++ dic_example.py
@@ -0,0 +1,18 @@
+#uma variável dicionário onde seus índices apontam pra outro dicionário
+all_guests = {'Alice':{'apples':5,'pretzels':12},
+              'Bob':{'ham sandwich':3,'apples':2},
+              'Carol':{'cups':3,'apple pies':1}}
+
+#método total_brought que recebe os convidados (guests) e os itens
+def total_brought(guests,item):
+    num_brought = 0
+    for k,v in guests.items():
+        num_brought = num_brought + v.get(item,0)
+    return num_brought
+
+print('Number of things being brought:')
+print('- Apples        '+str(total_brought(all_guests,'apples')))
+print('- Cups          '+str(total_brought(all_guests,'cups')))
+print('- Cakes         '+str(total_brought(all_guests,'cakes')))
+print('- Ham Sandwichs '+str(total_brought(all_guests,'ham sandwich')))
+print('- Apple pies    '+str(total_brought(all_guests,'apple pies')))
